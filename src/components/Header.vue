3b7d2d201efe425695dc27a254f6612965981ac6
aguinot58 <aymeric.guinot@gmail.com>
2024-09-04 11:27:44 +0200

    Mise en place system login-logout dans header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,21 +5,95 @@
           href="/"
           class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
         >
-          <span class="fs-4">Vue Template</span>
+          <img src="/src/assets/logo.png" />
+          <span class="fs-4">CodeBook - Admin</span>
         </a>
   
         <ul class="nav nav-pills">
           <li class="nav-item">
             <router-link to="/" class="nav-link">Home</router-link>
           </li>
+          <li class="nav-item">
+            <a
+              v-if="user"
+              @click="logout"
+              href="#"
+              class="nav-link"
+            >
+              Logout
+            </a>
+            <a
+              v-else
+              @click="login"
+              href="#"
+              class="nav-link"
+            >
+              Login
+          </a>
+        </li>
         </ul>
       </header>
     </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { auth } from '../firebase/index.js';
+import { signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
+
+export default {
+  setup() {
+    const user = ref(null);
+
+    // Vérifier l'état de l'utilisateur à chaque chargement
+    onMounted(() => {
+      onAuthStateChanged(auth, (currentUser) => {
+        user.value = currentUser;
+      });
+    });
+
+    // Fonction pour se connecter
+    const login = async () => {
+      const provider = new GoogleAuthProvider();
+      try {
+        await signInWithPopup(auth, provider);
+      } catch (error) {
+        console.error("Erreur de connexion : ", error);
+      }
+    };
+
+    // Fonction pour se déconnecter
+    const logout = async () => {
+      try {
+        await signOut(auth);
+      } catch (error) {
+        console.error("Erreur de déconnexion : ", error);
+      }
+    };
+
+    return {
+      user,
+      login,
+      logout,
+    };
+  }
+};
+</script>
   
 <style>
     body {
         padding: 0;
         margin: 0;
+    }
+
+    img {
+      width: 3em;
+      margin-right: 0.5em;
+    }
+
+    .nav-link {
+      font-size: 1.2em;
+      cursor: pointer;
+      color: black;
     }
 </style>
